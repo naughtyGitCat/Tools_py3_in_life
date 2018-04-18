ee6bde9ee1ea405b7d9ab63dc8ceacda44ad3558
psyduck007 <psyduck007@outlook.com>
2018-04-18 15:03:13 +0800

    更改格式

--- calender/SINGLE_JSON2DICT.py
+++ calender/SINGLE_JSON2DICT.py
@@ -1,7 +1,8 @@
 import json
 
+
 def getmailJSON():
-    with open('mail.json', 'rt',encoding="utf-8") as open_file:
+    with open('mail.json', 'rt', encoding="utf-8") as open_file:
         context = open_file.read()
     JSON_CONTEXT = json.loads(context.encode("utf-8"))
     print(JSON_CONTEXT)
@@ -11,4 +12,4 @@ def getmailJSON():
     TO = JSON_CONTEXT['TO']
     NAME = JSON_CONTEXT['NAME']
     #print('发信人：{},\n密码：{},\n主题：{},\n收件人：{}'.format(FROM,PASSWORD,SUBJECT,TO))
-    return  FROM,PASSWORD,SUBJECT,TO,NAME
+    return FROM, PASSWORD, SUBJECT, TO, NAME
