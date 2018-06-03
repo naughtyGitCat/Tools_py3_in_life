df477be19d82eed54470d3b67d5410e90bd57e36
NaughtyGitCat <the2ndhitler@163.com>
2018-06-03 23:04:00 +0800

    20180603

--- docopt/1.py
+++ docopt/1.py
@@ -1,16 +1,48 @@
-"""
+# coding=utf-8
+"""1
+
 Usage:
-  1.py init_instance [--configSvr] [--replname] [--port=<port>]
-  1.py serial <port> [--baud=9600] [--timeout=<seconds>]
-  1.py -h | --help | --version
+  1.py D_IP init_instance [--configSvr] [--replname=<replSetName>] [-P PORT | --port=PORT] [--disk=data{}]
+  1.py init_mongos [-P PORT | --port=PORT] [--instance=<IP>:<PORT>]
+  1.py -h | --help
+
+Arguments:
+  D_IP  destination IP
+
 Options:
-  --port=<port>   self port
+  -P PORT   Self port.
+  --disk=DATA_NUMBER    Install in /Data{}.
+  --replname=<replSetName> ReplSet`s Name.
+
 """
 from docopt import docopt
+
+def concat_parameters(dict_a):
+    opt_str = ''
+    for i in dict_a.keys():
+        # 如果该选项非false(有值，或True)，且是--parameters，追加拼接到参数字符串中
+        if dict_a[i]  and i[0:2] == '--':
+            print('key:', i)
+            opt_str = opt_str+' {}={}'.format(i,dict_a[i])
+    print(opt_str)
+
+
 
 
 if __name__ == '__main__':
     arguments = docopt(__doc__, version='0.1.1rc')
-    print(arguments)
+    # print(arguments)
+
+    if arguments['init_instance']:
+        D_IP = arguments['D_IP']
+        concat_parameters(arguments)
+
+    elif arguments['init_mongos']:
+        pass
+
+
+
+
+
 
 #python 1.py init_instance --port=3304
